c59066329fbcb338a120e8189be93ee844c30044
Valdiq <stasyshyn.vld@gmail.com>
2024-11-15 15:34:04 +0100

    redis config

--- src/main/java/org/example/couplemovie/CoupleMovieApplication.java
+++ src/main/java/org/example/couplemovie/CoupleMovieApplication.java
@@ -4,9 +4,11 @@ import org.example.couplemovie.properties.OMDBProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableConfigurationProperties(OMDBProperties.class)
+@EnableCaching
 public class CoupleMovieApplication {
 
     public static void main(String[] args) {
