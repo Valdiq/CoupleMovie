afdd0bf72a01d6b2befa0f586cc9420adc6eb2e3
Valdiq <stasyshyn.vld@gmail.com>
2024-11-15 15:46:46 +0100

    r2dbc config

--- src/main/java/org/example/couplemovie/CoupleMovieApplication.java
+++ src/main/java/org/example/couplemovie/CoupleMovieApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 @EnableConfigurationProperties(OMDBProperties.class)
 @EnableCaching
+@EnableR2dbcRepositories
 public class CoupleMovieApplication {
 
     public static void main(String[] args) {
