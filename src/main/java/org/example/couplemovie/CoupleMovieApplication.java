04a3a79988150a7ae65c435c363de3bd1ef00a6c
Valdiq <stasyshyn.vld@gmail.com>
2024-11-15 15:15:01 +0100

    properties config and docker compose set up

--- src/main/java/org/example/couplemovie/CoupleMovieApplication.java
+++ src/main/java/org/example/couplemovie/CoupleMovieApplication.java
@@ -1,9 +1,12 @@
 package org.example.couplemovie;
 
+import org.example.couplemovie.properties.OMDBProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(OMDBProperties.class)
 public class CoupleMovieApplication {
 
     public static void main(String[] args) {
